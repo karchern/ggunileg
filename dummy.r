8d7d1a173a60c8212664abb127122ed03e0ec06f
Nicolai Karcher <karchernic@gmail.com>
2023-08-13 21:37:10 +0200

    exposing numShapeLevels

--- dummy.r
+++ dummy.r
@@ -1,18 +1,17 @@
 library(ggplot2)
 library(tidyverse)
 
-
 someData <- data.frame(x = 1:100 + runif(n = 100, min = -10, max = 10), y = 1:100 + runif(n = 100, min = -10, max = 10), label1 = sample(letters[1:10], size = 100, replace = T), label2 = sample(1:5, size = 100, replace = T)) %>%
     mutate(label2 = as.factor(label2))
 
-manual_pal <- function(numShapeLevels, values = 1000) {
-    force(values)
+manual_pal <- function(numShapeLevels) {
+    values <- 1000
     function(n) {
         rep(1:numShapeLevels, length.out = values)
     }
 }
 
-scale_color_highres <- function(...) {
+scale_color_highres <- function(numShapeLevels, ...) {
     list(discrete_scale(
         "color",
         scale_name = "discreteColor",
@@ -21,7 +20,7 @@ scale_color_highres <- function(...) {
     discrete_scale(
         "shape",
         scale_name = "discreteShape",
-        palette = manual_pal(),
+        palette = manual_pal(numShapeLevels),
         ...))
 }
 
@@ -30,9 +29,3 @@ plot <- ggplot(data = someData, aes(x = x, y = y, color = label1, shape = label1
     scale_color_highres(numShapeLevels = 2) +
     theme_classic()
 ggsave(plot = plot, filename = "tmp/example_plot.pdf", width = 5, height = 5)
-
-outerFun <- function(...){
-    function() {
-        print(1:n)
-    }
-}
