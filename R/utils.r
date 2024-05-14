c32e29502ae6c4d0daf6943c0c548a7a805d7420
Nicolai Karcher <karchernic@gmail.com>
2024-05-14 11:29:24 +0200

    renaming numShapeLevels to num_shape_levels

--- R/utils.r
+++ R/utils.r
@@ -1,8 +1,8 @@
 #' Helper function for scale_color_highres
-manual_shape_pal <- function(numShapeLevels) {
+manual_shape_pal <- function(num_shape_levels) {
     values <- 1000
     function(n) {
-        rep(1:numShapeLevels, length.out = values)
+        rep(1:num_shape_levels, length.out = values)
     }
 }
 
@@ -12,19 +12,23 @@ manual_shape_pal <- function(numShapeLevels) {
 #' including a well-formatted legend that shows both color- and shape information for values
 #' (an approach I often use to distinguish > 6 or so different groups).
 #'
-#' @param numShapeLevels number of distinct shapes to use
+#' @param num_shape_levels number of distinct shapes to use
 #' @param colorPalette expects a color scale (such as those provided by scales::hue_pal() or function(x) RColorBrewer::brewer.pal(x, "Set1")),
 #' @return NULL
 #' @export
-scale_color_highres <- function(numShapeLevels, colorPalette = scales::hue_pal(), ...) {
-    list(discrete_scale(
-        "color",
-        scale_name = "discreteColor",
-        palette = colorPalette,
-        ...),
-    discrete_scale(
-        "shape",
-        scale_name = "discreteShape",
-        palette = manual_shape_pal(numShapeLevels),
-        ...))
+scale_color_highres <- function(num_shape_levels, colorPalette = scales::hue_pal(), ...) {
+    list(
+        discrete_scale(
+            "color",
+            scale_name = "discreteColor",
+            palette = colorPalette,
+            ...
+        ),
+        discrete_scale(
+            "shape",
+            scale_name = "discreteShape",
+            palette = manual_shape_pal(num_shape_levels),
+            ...
+        )
+    )
 }
